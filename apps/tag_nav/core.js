02fc2d555dec9924acb6fdc8518856f495d30ac0
Ido Ran <ido.ran@gmail.com>
2010-11-03 23:35:44 +0200

    first commit

---
+++ apps/tag_nav/core.js
@@ -0,0 +1,27 @@
+// ==========================================================================
+// Project:   TagNav
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals TagNav */
+
+/** @namespace
+
+  My cool new app.  Describe your application.
+  
+  @extends SC.Object
+*/
+TagNav = SC.Application.create(
+  /** @scope TagNav.prototype */ {
+
+  NAMESPACE: 'TagNav',
+  VERSION: '0.1.0',
+
+  // This is your application store.  You will use this store to access all
+  // of your model data.  You can also set a data source on this store to
+  // connect to a backend server.  The default setup below connects the store
+  // to any fixtures you define.
+  store: SC.Store.create().from(SC.Record.fixtures)
+  
+  // TODO: Add global constants or singleton objects needed by your app here.
+
+}) ;
---
+++ apps/tag_nav/main.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project:   TagNav
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals TagNav */
+
+// This is the function that will start your app running.  The default
+// implementation will load any fixtures you have created then instantiate
+// your controllers and awake the elements on your page.
+//
+// As you develop your application you will probably want to override this.
+// See comments for some pointers on what to do next.
+//
+TagNav.main = function main() {
+
+  // Step 1: Instantiate Your Views
+  // The default code here will make the mainPane for your application visible
+  // on screen.  If you app gets any level of complexity, you will probably 
+  // create multiple pages and panes.  
+  TagNav.getPath('mainPage.mainPane').append() ;
+
+  // Step 2. Set the content property on your primary controller.
+  // This will make your app come alive!
+
+  // TODO: Set the content property on your primary controller
+  // ex: TagNav.contactsController.set('content',TagNav.contacts);
+
+} ;
+
+function main() { TagNav.main(); }
