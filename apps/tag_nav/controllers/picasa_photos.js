d1e06ae02c88b48bf1786f6aebae589a5f1353b2
Ido Ran <ido.ran@gmail.com>
2010-11-21 13:34:19 +0200

    Replace slimbox with fancybox which works much better inside SC app

--- apps/tag_nav/controllers/picasa_photos.js
+++ apps/tag_nav/controllers/picasa_photos.js
@@ -16,39 +16,39 @@ TagNav.picasaPhotosController = SC.ArrayController.create(
   selectedPhoto: null,
   selectedPhotoBinding: SC.Binding.single('TagNav.picasaPhotosController.selection'),
 
-  slimbox_config: {
-            loop: false,
-            overlayOpacity: 0.6,
-            overlayFadeDuration: 400,
-            resizeDuration: 400,
-            resizeEasing: "swing",
-            initialWidth: 250,
-            initlaHeight: 250,
-            imageFadeDuration: 400,
-            captionAnimationDuration: 400,
-            counterText: "{x}/{y}",
-            closeKeys: [27, 88, 67, 70],
-            prevKeys: [37, 80],
-            nextKeys: [39, 83]
-        }, //-- overrule defaults is needed
-
-	photoClicked: function() {
-		var photoInfo = this.get('selectedPhoto');
-		jQuery.slimbox(photoInfo.get('largeUrl'), photoInfo.get('title'), this.slimbox_config);
-	  },
+  photoClicked_single: function() {
+    var photoInfo = this.get('selectedPhoto');
+    $.fancybox({
+            //'orig'            : $(this),
+            'padding'       : 0,
+            'href'          : photoInfo.get('largeUrl'),
+            'title'         : photoInfo.get('titke'),
+            'transitionIn'  : 'elastic',
+            'transitionOut' : 'elastic'
+    });
+  },
 		
-  photoClicked_all_photos: function() {
+  photoClicked: function() {
 	var photoInfo = this.get('selectedPhoto');
 	var albumInfo = TagNav.picasaAlbumController.get('content');
 	
 	var photoArray = [];
 	for (var i=0; i<albumInfo.photos.length; i++) {
 		var p = albumInfo.photos[i];
-		var photoAndTitle = [p.get('largeUrl'), p.get('title')];
+		var photoAndTitle = { href: p.get('largeUrl'), title: p.get('title') };
 		photoArray.pushObject(photoAndTitle);
 	}
 	var currPhotoIndex = albumInfo.photos.indexOf(photoInfo);
 	
-	jQuery.slimbox(photoArray, currPhotoIndex, this.slimbox_config);
+	$.fancybox(photoArray,
+		{
+        //'orig'            : $(this),
+        'padding'       : 0,
+        'transitionIn'  : 'elastic',
+        'transitionOut' : 'elastic',
+        'type': 'image',
+        'changeFade': 0,
+        'index': currPhotoIndex
+    });
   }
 }) ;
